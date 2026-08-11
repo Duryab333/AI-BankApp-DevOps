e33b3410c433f095ae31030bc0fbbe9292730390
Duryab333 <duryabtariq333@gmail.com>
2026-08-11 13:59:34 +0200

    Fixes: bugs in code

--- src/main/java/com/example/bankapp/repository/TransactionRepository.java
+++ src/main/java/com/example/bankapp/repository/TransactionRepository.java
@@ -5,6 +5,14 @@ import org.springframework.data.jpa.repository.JpaRepository;
 
 import java.util.List;
 
+//public interface TransactionRepository extends JpaRepository<Transaction, Long> {
+//    List<Transaction> findByAccountIdOrderByTimestampDesc(Long accountId);
+//}
+
+import org.springframework.data.jpa.repository.Query;
+import org.springframework.data.repository.query.Param;
+
 public interface TransactionRepository extends JpaRepository<Transaction, Long> {
-    List<Transaction> findByAccountIdOrderByTimestampDesc(Long accountId);
+    @Query("SELECT t FROM Transaction t WHERE t.account.id = :accountId ORDER BY t.timestamp DESC")
+    List<Transaction> findByAccountIdOrderByTimestampDesc(@Param("accountId") Long accountId);
 }
